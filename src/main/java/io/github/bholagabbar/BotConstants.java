b99f2db5379ff03037b6684122f9acfb627bfee0
bholagabbar <dev.shreyans96@gmail.com>
2016-11-02 23:59:52 +0530

    updated to ull-slack, automatically retreiving team name

--- src/main/java/io/github/bholagabbar/BotConstants.java
+++ src/main/java/io/github/bholagabbar/BotConstants.java
@@ -52,11 +52,9 @@ public class BotConstants {
             SLACK_CHANNEL = prop.getProperty("slackChannel");
             SLACK_BOT_NAME = prop.getProperty("slackBotName");
             SLACK_MODE_ADMIN = prop.getProperty("slackModeAdmin");
-            SLACK_TEAM_NAME = prop.getProperty("slackTeamName");
             MODE = Integer.parseInt(prop.getProperty("mode"));
 
             //Messages
-            IRC_JOIN_MSG = "@everyone Hello! I'm " + IRC_BOT_NAME + ", the bot relaying messages between this IRC Channel and the Slack Channel '" + SLACK_CHANNEL + "' of your Slack team " + SLACK_TEAM_NAME + " Query ' @" + SLACK_BOT_NAME + " slack_users' to get a list of users on the Slack Channel and directly ping them by @username on Slack. Star me at http://github.com/bholagabbar/irackbot :)";
             SLACK_JOIN_MSG = "@channel Hello! I'm" + SLACK_BOT_NAME + ", the bot relaying messages between this Slack Channel and the IRC Channel '" + IRC_CHANNEL + "' Query ' @" + IRC_BOT_NAME + " irc_users' to get a list of users on the IRC Channel and directly ping them by @username on IRC. Star me at http://github.com/bholagabbar/irackbot :)";
             SLACK_TP_MSG = "I don't read a valid command there. I'm alive and kicking though, if that's what you wanted to know. Type ' @" + BotConstants.SLACK_BOT_NAME + " irc_users ' to get a list of users on the linked IRC Channel, " + BotConstants.IRC_CHANNEL;
             SLACK_MODE_1_TO_2_MESSAGE = "Slack Mode has been changed from 1 to 2 by the admin. The Slack users will only receive messages on a mention of their username and otherwise. All communication from Slack to IRC is still active";
@@ -68,4 +66,17 @@ public class BotConstants {
             inputStream.close();
         }
     }
+
+    public static void setSlackChannelObject(SlackChannel slackChannel) {
+        SLACK_CHANNEL_OBJECT = slackChannel;
+    }
+
+    public static void setSlackTeamName(String teamName) {
+        SLACK_TEAM_NAME = teamName;
+    }
+
+    public static void setJoinIRCMessage() {
+        IRC_JOIN_MSG = "@everyone Hello! I'm " + IRC_BOT_NAME + ", the bot relaying messages between this IRC Channel and the Slack Channel '" + SLACK_CHANNEL + "' of your Slack team " + SLACK_TEAM_NAME + " Query ' @" + SLACK_BOT_NAME + " slack_users' to get a list of users on the Slack Channel and directly ping them by @username on Slack. Star me at http://github.com/bholagabbar/irackbot :)";IRC_JOIN_MSG = "@everyone Hello! I'm " + IRC_BOT_NAME + ", the bot relaying messages between this IRC Channel and the Slack Channel '" + SLACK_CHANNEL + "' of your Slack team " + SLACK_TEAM_NAME + " Query ' @" + SLACK_BOT_NAME + " slack_users' to get a list of users on the Slack Channel and directly ping them by @username on Slack. Star me at http://github.com/bholagabbar/irackbot :)";
+    }
+
 }
--- src/main/java/io/github/bholagabbar/Main.java
+++ src/main/java/io/github/bholagabbar/Main.java
@@ -1,12 +1,14 @@
 package io.github.bholagabbar;
 
+import static io.github.bholagabbar.BotConstants.*;
+
 public class Main {
 
     public static IRCBot ircBot;
     public static SlackBot slackBot;
 
     private static void setupIRCBot() throws Exception {
-        ircBot = new io.github.bholagabbar.IRCBot(BotConstants.IRC_BOT_NAME);
+        ircBot = new io.github.bholagabbar.IRCBot(IRC_BOT_NAME);
         ircBot.setVerbose(true);
         ircBot.connect(BotConstants.IRC_SERVER);
         ircBot.joinChannel(BotConstants.IRC_CHANNEL);
@@ -16,8 +18,12 @@ public class Main {
     private static void setupSlackBot() throws Exception {
         slackBot = new SlackBot();
         slackBot.session.connect();
-        slackBot.session.joinChannel(BotConstants.SLACK_CHANNEL);
-        BotConstants.SLACK_CHANNEL_OBJECT = slackBot.session.findChannelByName(BotConstants.SLACK_CHANNEL);
+        slackBot.session.joinChannel(SLACK_CHANNEL);
+        //Set Dependant Bot Constants
+        BotConstants.setSlackChannelObject(slackBot.session.findChannelByName(SLACK_CHANNEL));
+        BotConstants.setSlackTeamName(slackBot.session.getTeam().getName());
+        BotConstants.setJoinIRCMessage();
+        //Start
         slackBot.Listen();
         slackBot.session.sendMessage(BotConstants.SLACK_CHANNEL_OBJECT, BotConstants.SLACK_JOIN_MSG);
     }
